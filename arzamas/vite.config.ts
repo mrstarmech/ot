11c64061dc654d646b416c74d4c7d49883e8abca
mrstarmech <mr.starmech@gmail.com>
2022-07-15 18:01:48 +0700

    upd

--- arzamas/vite.config.ts
+++ arzamas/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ command }) => {
   return {
     plugins: [
       vue(),
-      //ViteWebfontDownload(),
+      ViteWebfontDownload(),
       viteImagemin({
         gifsicle: {
           optimizationLevel: 7,
